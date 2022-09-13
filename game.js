75a8fc524ae46db089a04385aa0ae0d7e7b37014
onur <kizilayonurtahsin@gmaik.com>
2022-09-13 20:55:30 +0300

    v2

--- game.js
+++ game.js
@@ -2,3 +2,48 @@
 //*  GUESS MY NUMBER
 //*  Game Logic
 //*======================================
+
+//? 1-100 arasinda rasgele bir sayi tut.
+
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
+//? variables
+let score = 10;
+let topScore = 0;
+//* CheckBtn basildiginda kontrolleri yap
+document.querySelector(".check-btn").addEventListener("click", () => {
+  const guessInput = Number(document.querySelector(".guess-input").value);
+  const msg = document.querySelector(".msg");
+  const body = document.querySelector("body");
+  //? eger input girilmediyse Kullaniciya uyari ver.
+
+  if (!guessInput) {
+    msg.innerText = "Please enter a number";
+    //! eger rasgele == input.value
+  } else if (randomNumber === guessInput) {
+    msg.innerHTML = `Congrats You Win <i class="fa-sharp fa-solid fa-trophy fa-2x"></i>`;
+    body.style.background = " crimson";
+    if (score > topScore) {
+      topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
+    }
+  }
+});
+
+//? tebrikler bildiniz.
+//? background = green
+//? eger score > topScore
+//?     topScore = score
+//? secret_number = gorunur.
+
+//! değilse
+//! eger score > 0
+//!   score = score -1
+//?   eğer rasgele < input.value
+//?     AZALT
+//?   degilse
+//?     ARTTIR
+//! degise
+//? Uzgunuz kaybetiniz.
+
+//* againBtn basildiginda kontrolleri yap
