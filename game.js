22b1541f549164bc00507806b1f2f20a88de4a89
onur <kizilayonurtahsin@gmaik.com>
2022-09-13 23:03:39 +0300

    v4

--- game.js
+++ game.js
@@ -5,11 +5,17 @@
 
 //? 1-100 arasinda rasgele bir sayi tut.
 
-const randomNumber = Math.round(Math.random() * 100);
+let randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 //? variables
 let score = 10;
-let topScore = 0;
+// let topScore = 0;
+//! local stroage den topscore adıyla bir değişgen oluşturur
+//* tanımlı bir topScore varsa onu oku yoksa 0 koy
+let topScore = localStorage.getItem("topScore") || 0;
+//? domdaki top-score değerini localStorage dan okuyarak güncelle
+document.querySelector(".top-score").textContent = topScore;
+console.log(topScore);
 //* CheckBtn basildiginda kontrolleri yap
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
@@ -22,10 +28,14 @@ document.querySelector(".check-btn").addEventListener("click", () => {
     //! eger rasgele == input.value
   } else if (randomNumber === guessInput) {
     msg.innerHTML = `Congrats You Win <i class="fa-sharp fa-solid fa-trophy fa-2x"></i>`;
-    body.style.background = " crimson";
+    body.className = "bg-warning";
+    document.querySelector(".check-btn").disabled = true;
     if (score > topScore) {
-      topScore = score;
-      document.querySelector(".top-score").textContent = topScore;
+      //   topScore = score;
+      //  ? localStorage daki topScore değişgenini güncelle
+      localStorage.setItem("topScore", score);
+      //  ? domdaki top-score değerini güncelle
+      document.querySelector(".top-score").textContent = score;
     }
     document.querySelector(".secret-number").textContent = randomNumber;
   } else {
@@ -34,17 +44,48 @@ document.querySelector(".check-btn").addEventListener("click", () => {
       guessInput > randomNumber
         ? (msg.innerHTML = `DECREASE <i class="fa-solid fa-arrow-trend-down fa-2x"></i>`)
         : (msg.innerHTML = `INCREASE <i class="fa-solid fa-arrow-trend-up fa-2x"></i>`);
+      document.querySelector(".guess-input").value = "";
     } else {
       msg.innerHTML = `You Lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
       document.querySelector(".secret-number").textContent = randomNumber;
       document.querySelector(".check-btn").disabled = true;
-      body.style.background = " black";
+      body.className = "bg-dark";
     }
     document.querySelector(".score").textContent = score;
   }
 });
 
 //* againe basildiginda kontrolleri yap
+document.querySelector(".again-btn").addEventListener("click", () => {
+  score = 10;
+  document.querySelector(".score").textContent = score;
+  randomNumber = Math.round(Math.random() * 100);
+  document.querySelector(".secret-number").textContent = "?";
+  console.log(randomNumber);
+  document.querySelector(".check-btn").disabled = false;
+  document.querySelector("body").classList.remove("bg-warning", "bg-dark");
+  document.querySelector(".guess-input").value = "";
+  document.querySelector(".msg").innerText = "Starting...";
+});
+
+document.querySelector(".guess-input").addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    document.querySelector(".check-btn").click();
+  }
+});
+//* againBtn basildiginda kontrolleri yap
+document.querySelector(".again");
+// localStorage sessionStorage
+// myObj = { a: 1, b: 2, c: 3 };
+// localStorage.setItem("OBJ", JSON.stringify(myObj));
+// const readObj = localStorage.getItem("OBJ");
+// const readOBJ = JSON.parse(localStorage.getItem("OBJ"));
+// console.log(readObj);
+// console.log(typeof readObj);
+// console.log(typeof readOBJ);
+// console.log(typeof readOBJ);
+
+//! puseduo
 
 //? tebrikler bildiniz.
 //? background = green
@@ -61,6 +102,3 @@ document.querySelector(".check-btn").addEventListener("click", () => {
 //?     ARTTIR
 //! degise
 //? Uzgunuz kaybetiniz.
-
-//* againBtn basildiginda kontrolleri yap
-document.querySelector(".again");
